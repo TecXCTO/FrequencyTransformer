f2b074a2447b29843d28806e19eb42f0ea1c9640
TecXCTO <cto.tecx@gmail.com>
2026-02-26 08:43:38 +0530

    Update frequency_calibration.py

--- frequency_calibration.py
+++ frequency_calibration.py
@@ -6,7 +6,17 @@ class FrequencyCalibration(FrequencyTransformer):
     super.__init__()
     self.
 
-def frequency_calibrator(self, primitive_frequency):
+def frequency_calibratoin(self, primitive_frequency):
   
   while primitive_frequency:
     pf
+
+class FrequencyCalibrator(FrequencyCalibration):
+  def __init__(self, *args, **kwargs, args=[]):
+    super.__init__()
+    self.
+
+def frequency_calibrator(self, primitive_frequency):
+   calibrated_frequency = input_frequency
+  while calibrated_frequency>primitive_frequency or  calibrated_frequency>primitive_frequency:
+    
